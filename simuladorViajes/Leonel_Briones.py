c911b1a5763ade4c94583cce12bf4ece9ebee7b2
Jarod Smith <lbriones.dev@gmail.com>
2024-05-15 12:23:02 -0400

    Agrega avance juego ahorcado y avance simulador de viajes

--- simuladorViajes/Leonel_Briones.py
+++ simuladorViajes/Leonel_Briones.py
@@ -6,13 +6,14 @@ PARIS = None
 LONDRES = None
 ROMA = None
 
+# Variables para guardar los costos
+costoVuelo = 0
+costoAlojamiento = 0
+costoTours = 0
 
-""" cantidadViajeros = input("[+] Ingrese la cantidad de viajeros: ")
-valorPasajePersona = input("[+] Ingrese el valor del pasaje por persona (USD): ")
-cantidadNochesPorDestino = input("[+] Ingrese la cantidad de noches por destino: ")
-valorNoche = input("[+] Ingrese el valor por noche (USD): ")
-cantidadTour = input("[+] Ingrese la cantidad de tours a realizar: ")
-valorTour = input(f"[+] Ingrese el valor por tour (USD): ") """
+# 
+destinosRecorrer = ""
+cantidadDestinos = 0
 
 # FUNCIONAMIENTO DEL PROGRAMA
 
@@ -43,7 +44,7 @@ print("""\n*** SELECCIÓN DE DESTINOS ***\n
       4. Roma
       """)
 
-
+# Setea los destinos para el menú
 for i in range(cantidadDestinos):
  
     esError = True
@@ -58,11 +59,15 @@ for i in range(cantidadDestinos):
             else:
                 if opcion == 1 and MADRID == None:
                     MADRID = True
+                    destinosRecorrer += "Madrid, "
                 elif opcion == 2 and PARIS == None:
                     PARIS = True
+                    destinosRecorrer += "París, "
                 elif opcion == 3 and LONDRES == None:
                     LONDRES = True
+                    destinosRecorrer += "Londres, "
                 elif opcion == 4 and ROMA == None:
+                    destinosRecorrer += "Roma, "
                     ROMA = True
                 else:
                     # Si la opción ya fue seleccionada, se muestra un mensaje de error
@@ -87,11 +92,111 @@ for i in range(cantidadDestinos):
                     destinos = destinos + f"4. Roma\n"
                 # Si ya se seleccionaron los 4 destinos, se muestra un mensaje
                 if i == 3:
-                    destinos = "[!] No hay destinos disponibles."
+                    destinos = "[!] Ha seleccionado los 4 destinos disponibles"
                 # Muestra destinos disponibles
                 print(destinos)
                 # Si la opción es correcta, se sale del bucle
                 esError = False
+                            
+# Elimina los últimos 2 caracteres de la cadena de destinos (LIMPIA)
+destinosRecorrer = destinosRecorrer[:-2].strip()
+print(destinosRecorrer)
 
-# Ingreso y validación de la cantidad de viajeros
-cantViajeros = input("[+] Ingrese la cantidad de viajeros: ")
+#TODO: DEPENDIENDO DE CANDIDAD DE DESTINOS HACER UN FOR CON LOS INGRESOS DE DATOS
+for i in range(1, cantidadDestinos):
+
+    # Ingreso y validación de la cantidad de viajeros
+    esError = True
+    while esError:
+        # Ingreso de la cantidad de viajeros
+        cantViajeros = input("[+] Ingrese la cantidad de viajeros: ")
+        try:
+            # Cambiamos el tipo de dato de la cantidad de viajeros
+            cantViajeros = int(cantViajeros)
+            # TODO: CREAR UNA LISTA PARA GUARDAR LA CANTIDAD DE VIAJEROS
+            # Si la cantidad de viajeros es correcta, se sale del bucle
+            esError = False
+        except ValueError:
+            print("[!] ERROR: Ingrese sólo valores Numéricos")
+    
+    print(f"[!] Lugar de destino {i}") # MOSTRAR EL DESTINO 1 QUE ESTÁ EN LA VARIABLE
+    # TODO: CREAR UNA LISTA PARA AGREGAR EL DESTINO AL OUTPUT
+
+    # Ingreso y validación del valor del pasaje por persona
+    esError = True
+    while esError:
+        # Ingresa el valor del pasaje por persona
+        valorPasaje = input("[+] Ingrese el valor del pasaje por persona (USD): ")
+        try:
+            # Cambiamos el tipo de dato del valor del pasaje
+            valorPasaje = float(valorPasaje)
+            # TODO: CREAR LISTA PARA GUARDAR EL VALOR DEL PASAJE POR PERSONA
+            # Si la cantidad de viajeros es correcta, se sale del bucle
+            esError = False
+        except ValueError:
+            print("[!] ERROR: Ingrese sólo valores Numéricos")
+            
+    # Ingreso y validación de la cantidad de noches por destino
+    esError = True
+    while esError:
+        # Ingresa el valor de las noches por destino
+        cantNoches = input("[+] Ingrese la cantidad de noches por destino: ")
+        try:
+            # Cambiamos el tipo de dato de la cantidad de noches
+            cantNoches = int(cantNoches)
+            # TODO: CREAR UNA LISTA PARA GUARDAR LAS NOCHES POR DESTINO
+            # Si la cantidad de viajeros es correcta, se sale del bucle
+            esError = False
+        except ValueError:
+            print("[!] ERROR: Ingrese sólo valores Numéricos")
+            
+    # Ingreso y validación del valor por noche
+    esError = True
+    while esError:
+        # Ingresa el valor por noche
+        valorNoche = input("[+] Ingrese el valor por noche (USD): ")
+        # Cambiamos el tipo de dato del valor por noche
+        try:
+            valorNoche = float(valorNoche)
+            # TODO: CREAR UNA LISTA PARA GUARDAR EL VALOR POR NOCHE
+            # Si la cantidad de viajeros es correcta, se sale del bucle
+            esError = False
+        except ValueError:
+            print("[!] ERROR: Ingrese sólo valores Numéricos")
+            
+    # Ingreso y validación de la cantidad de tours
+    esError = True
+    while esError:
+        # Ingresa la cantidad de tours
+        cantTours = input("[+] Ingrese la cantidad de tours a realizar: ")
+        # Cambiamos el tipo de dato de la cantidad de tours
+        try:
+            cantTours = int(cantTours)
+            while cantTours > 0:
+                valorTour = input(f"[+] Ingrese el valor por tour {cantTours} (USD): ")
+                try:
+                    valorTour = float(valorTour)
+                    if valorTour < 0:
+                        print("[!] ERROR: El valor del tour no puede ser negativo.")
+                    else:
+                        # Solo cuando se ingresa un valor correcto, se resta 1 a la cantidad de tours
+                        cantTours = cantTours - 1
+                        # TODO: CREAR UN ACUMULADOR PARA GUARDAR EL VALOR DE LOS TOURS
+                        # Si la cantidad de viajeros es correcta, se sale del bucle
+                        esError = False
+                except ValueError:
+                    print("[!] ERROR: Ingrese sólo valores Numéricos")
+        except ValueError:
+            print("[!] ERROR: Ingrese sólo valores Numéricos")
+            
+    # Cálculo de los costos
+    output = f"""*************************************
+    Los destinos que recorrerás son: {destinosRecorrer}
+    El valor por conceptos de alojamiento es de: {costoAlojamiento} USD
+    El valor por concepto de vuelos es de: {costoVuelo} USD
+    El valor por concepto de tours es de: {costoTours} USD
+    **************************************************
+    """
+
+    # Imprime en terminal resumen de costos totales
+    print(output)
