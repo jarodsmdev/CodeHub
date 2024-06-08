9b1a84c1faa91549cacf9dfd535a7b0ddedc2a13
Jarod Smith <lbriones.dev@gmail.com>
2024-06-08 10:07:21 -0400

    Agrega ejercicio 2C GuiaDiccionarios

---
+++ GuiaDiccionarios/GD_E2C.py
@@ -0,0 +1,167 @@
+"""
+Ejercicio 2B:
+Trabajaremos con un diccionario que tiene como llaves poderes, y como valores listas de nombres de superhéroes.
+
+A. Agregar Superhéroe a Poder
+
+Haga un programa que permita ingresar un poder y un nombre de superhéroe, y agregue ese superhéroe a la lista de ese poder. Considere que si el poder no existe en el diccionario, debe crearlo junto con la lista y el superhéroe recién ingresado por teclado. Si el poder existe y el superhéroe también, entonces no debe ingresarlo para que así no se repitan nombres.
+
+B. Eliminar Superhéroe
+
+Haga un programa que permita ingresar por teclado un superhéroe y elimine ese superhéroe de todo el diccionario.
+
+C. Consultar Superhéroes por Poderes
+
+Haga un programa que permita ingresar por teclado 2 poderes y entregue una lista con todos los superhéroes que tienen al menos uno de esos poderes. Los nombres en la lista no deben repetirse.
+"""
+
+# Función que verifica si el poder (key) existe en el diccionario
+def existePoder(poder, diccionario):
+    if poder in diccionario:
+        return True
+    else:
+        return False
+
+# Función que verifica si el superhéroe existe en la lista de un poder(key)
+def existeSuperheroe(superheroe, diccionario, poder):
+    for nombre in diccionario[poder]:
+        if nombre == superheroe:
+            return True
+    return False
+
+# Función que elimina un superhéroe de todo el diccionario
+def eliminarSuperheroe(superheroe, diccionario):
+    eliminado = False
+    contador = 0
+    for poder in diccionario:
+        for s in diccionario[poder]:
+            if s == superheroe:
+                diccionario[poder].remove(superheroe)
+                eliminado = True
+                contador = contador + 1
+    if eliminado:
+        print(f"[!] Superhéroe {superheroe} eliminado correctamente, {contador} veces")
+    else:
+        print("[!] No se ha encontrado el superhéro a eliminar")
+    
+
+# Función que valida si un número es entero
+def validarNumeroEntero(numero):
+    try:
+        numero = int(numero)
+        return True
+    except:
+        return False
+    
+# Muestra por terminal diccionario sin formato
+def mostrarDiccionario(diccionario):
+    print(diccionario)
+
+# Función que agrega un superhéroe a un poder (key) del diccionario
+def agregarSuperheroe():
+    # Solicitar al usuario el poder y el superhéroe
+    poder = input("[+] Ingrese un poder: ").capitalize()
+    superHeroe = input("[+] Ingrese un nombre de superhéroe: ").capitalize()
+
+    # Verificar si el poder Existe como clave en el diccionario
+    if existePoder(poder, poderes):
+        # Verfificar si existe el superhéroe con el poder
+        if not existeSuperheroe(superHeroe, poderes, poder):
+            # No existe superhéroe con ese poder en los values del diccionario. Se agrega a la lista
+            poderes[poder].append(superHeroe)
+            print(f"[!] Superhéroe {superHeroe} agregado correctamente a la lista {poder}")
+        else:
+            print(f"[!] Superhéroe {superHeroe} ya existe con el poder {poder}")
+    else:
+        # No existe se agrega como Key el poder y el nombre como value como una lista
+        poderes[poder] = [superHeroe]
+        print(f"[!] Poder: {poder}, Superhéroe: {superHeroe} agregado correctamente!")
+
+def filtrarPorPoderes(diccionario):
+    # Constante para el filtro de superhéroes
+    MAX_FILTRO_PODER = 2
+    
+    # Variables para el filtro
+    listaPoderes = []
+    listaSuperheroes = []
+    contador = 0
+
+    # Pedir 2 poderes para realizar el filtro de superhéroes, sin repetir
+    while contador < MAX_FILTRO_PODER:
+        # Solicitar poder al usuario
+        poder = input(f"[+] Ingrese poder {contador + 1} a filtrar: ").capitalize()
+        
+        # Verificar si el poder (key) ingresado existe en el diccionario
+        if not existePoder(poder, diccionario):
+            print("[!] ERROR: El poder ingresado no existe")
+        else:
+            # Verificar si el poder ya se encuentra en la lista de poderes (evitar repetidos)
+            if poder in listaPoderes:
+                print("[!] Poder ya se encuentra seleccionado")
+                print(listaPoderes)
+            else:
+                # Agregar poder a la lista de poderes
+                contador += 1
+                listaPoderes.append(poder)
+    # FIN WHILE
+
+    # Se crea un conjunto vacío, el cual recibirá los superhéroes filtrados
+    # Se utiliza un conjunto para evitar duplicados, ya que por definición no permite elementos repetidos
+    filtro = set()
+    
+    # Filtrar superhéroes que tengan al menos uno de los poderes seleccionados
+    for p in listaPoderes:
+        for p in diccionario[p]:
+            filtro.add(p)
+            
+    # Convertir el conjunto a lista
+    listaSuperheroes = list(filtro)
+    
+    # Retornar lista de superhéroes filtrados
+    return listaSuperheroes
+
+# Diccionario con los poderes y superhéroes
+poderes = {
+    "Volar": ["Superman", "Mujer maravilla", "Ironman"],
+    "Rayos": ["Superman", "Ironman", "Ciclope", "Capitana marvel"],
+    "Velocidad": ["Flash", "Superman"],
+    "Fuerza": ["Hulk", "Mujer maravilla", "Superman"],
+    "Inteligencia": ["Ironman", "Profesor-X"]
+}
+
+MENU = """
+1. Agregar Superhéroe a Poder
+2. Eliminar Superhéroe
+3. Consultar Superhéroes por Poderes
+4. Mostrar diccionario
+0. Salir
+"""
+
+### MAIN ###
+while True:
+    print(MENU)
+    opcion = input("[+] Ingrese una opción: ")
+    if validarNumeroEntero(opcion):
+        opcion = int(opcion)
+        if opcion == 1:
+            print("--- AGREGAR SUPERHÉROE ---\n")
+            agregarSuperheroe()
+        elif opcion == 2:
+            print("--- ELIMINAR SUPERHÉROE ---\n")
+            superheroe = input("[+] Ingrese nombre de superhéroe: ").capitalize()
+            eliminarSuperheroe(superheroe, poderes)
+        elif opcion == 3:
+            print("--- CONSULTAR SUPERHEROES ---\n")
+            print(filtrarPorPoderes(poderes))
+        elif opcion == 4:
+            print("--- MOSTRAR SUPERHÉROES ---\n")
+            mostrarDiccionario(poderes)
+        elif opcion == 0:
+            print("[!] Saliendo del programa")
+            break
+    else:
+        print("[!] ERROR: Ingrese un valor numérico")
+
+
+
+print("\n",poderes)
