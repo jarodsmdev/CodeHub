c15d8efe507f64bdf64297ef1c6f14712e7d0605
Jarod Smith <lbriones.dev@gmail.com>
2024-06-21 01:27:35 -0400

    Fix: Corrige el método de ordenamiento de la lista

--- Mutante/Leonel_Briones/EM1.py
+++ Mutante/Leonel_Briones/EM1.py
@@ -1,4 +1,3 @@
-
 import time, signal, sys
 
 # Ctrl+C
@@ -7,14 +6,41 @@ def def_handler(sig: int, frame: object):
     sys.exit(1)
     
 def imprimirPorcentaje(actual: int, total: int):
+    """
+    Imprime el porcentaje de completado de un proceso
+    
+    Args:
+        actual (int): Cantidad de elementos procesados
+        total (int): Cantidad total de elementos a procesar
+    """
     porcentaje = round((actual / total) * 100, 1)
     print(f"{porcentaje}% completado")
+    
 
-def ordenarListaMenorAMayor(lista: list) -> list:
+def ordenarLista(lista:list) -> list:
+    """
+    Implementación de ordenamiento de lista de listas
+    
+    Args:
+        lista (list): Lista de listas a ordenar
+    
+    Returns:
+        list: Lista de listas ordenada
+    """
+    for p in lista:
+        p.reverse()
+
+    lista.sort()
+    
+    for p in lista:
+        p.reverse()
+    return lista
+
+""" def ordenarListaMenorAMayor(lista: list) -> list:
     print("[!] Comenzando a ordenar la lista... paciencia")
     elemento = -1 # IMC Representa el ultimo elemento de la lista
     start = time.time()
-    time.sleep(1)
+    #time.sleep(1)
 
     for i in range(len(lista) - 1):
         imprimirPorcentaje(i, len(lista))
@@ -26,31 +52,56 @@ def ordenarListaMenorAMayor(lista: list) -> list:
     end = time.time()
     length = end - start
     print(f"[!] Ordenamiento finalizado.  Duración: {round(length)} segundos")
-    return lista
+    return lista """
+
+def calcularPromedioIMC(pacientes: list) -> float:
+    """
+    Calcula el promedio de IMC de una lista de pacientes
+    
+    Args:
+        pacientes (list): Lista de pacientes
+    
+    Returns:
+        float: Promedio de IMC
+    """
+    sumaIMC = 0.0
+    for paciente in pacientes:
+        sumaIMC += float(paciente[9])
+    promedio = sumaIMC / len(pacientes)
+    return round(promedio, 2)
 
 def promedioIMC_por_estado_de_salud(nombre_archivo: str, estado_salud) -> int:
     pacientes = leerArchivoCSV(nombre_archivo)
     filtroEstadoSalud = []
-    sumaIMC = 0.0
-    # Filtrar pacientes por estado_salud (posicion 0)
-    for p in pacientes:
-        if p[0] == estado_salud:
-            filtroEstadoSalud.append(p)
-            sumaIMC += float(p[9])
-    
-    # Calcular Promedio IMC
-    cantPacientes = len(filtroEstadoSalud)
-    if cantPacientes == 0:
-        promedio = 0
-    else:
-        promedio = round(sumaIMC / cantPacientes, 2)
-    
-    # Ordenar pacientes de menor a mayor
-    filtroEstadoSalud = ordenarListaMenorAMayor(filtroEstadoSalud)
-    exportarPacientes(filtroEstadoSalud, estado_salud)
-    return cantPacientes
 
-def leerArchivoCSV(archivo: str) -> int:
+    # Filtrar pacientes por estado_salud (posicion 0)
+    for i in range(len(pacientes)):
+        if pacientes[i][0] == estado_salud:
+
+            imc = float(pacientes[i][9])
+            pacientes[i].pop()
+            pacientes[i].append(imc)
+
+            filtroEstadoSalud.append(pacientes[i])
+    
+    """ print("Primeros 10 sin Ordenar")
+    for p in filtroEstadoSalud[0:10]:
+        print(p)
+    print("-"*10) """
+    
+    # Ordenar pacientes de menor a mayor IMC
+    listaOrdenada = ordenarLista(filtroEstadoSalud)
+    
+    """ print("Primeros 10 Ordenados")
+    for p in listaOrdenada[0:10]:
+        print(p) """
+    promedio = calcularPromedioIMC(listaOrdenada)
+    
+    # Exportar pacientes a archivo de texto
+    exportarPacientes(listaOrdenada, estado_salud, promedio)
+    return len(listaOrdenada)
+
+def leerArchivoCSV(archivo: str) -> list:
     pacientes = []
     with open(archivo, "r") as file:
         for linea in file:
@@ -58,10 +109,10 @@ def leerArchivoCSV(archivo: str) -> int:
             pacientes.append(paciente)
     return pacientes
 
-def exportarPacientes(listaPacientes: list, nombreArchivo: str):
-    file = open(nombreArchivo + ".txt", "w")
+def exportarPacientes(listaPacientes: list, nombreArchivo: str, promedio: float):
+    file = open(nombreArchivo + "-IMC.txt", "w")
     for paciente in listaPacientes:
-        file.write(f"{paciente[1]}: {paciente[9]}\n")
+        file.write(f"{paciente[1]}: {paciente[9]}: {promedio}\n")
     file.close()
     
 def main():
